10e2281106b919cc3f67e0f546704bb0fc248ce7
Muhamad Syah Reza <muhamadsyahr45@gmail.com>
2023-10-17 10:53:51 +0700

    Update NotesList.jsx

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -12,7 +12,7 @@ function NotesList({ notes, onDelete, onArchive, onUnArchive }) {
 	return (
 		<>
 			{notes.map((notes) => (
-				<div className="notes" key={notes.id} id={`notes-${notes.id}`}>
+				<div className="notes my-3" key={notes.id} id={`notes-${notes.id}`}>
 					<NoteItem
 						key={notes.id}
 						id={notes.id}
